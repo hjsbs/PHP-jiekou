f50b99674c27c44df883b75965634cd77d692ad8
hjsbs <59188658+hjsbs@users.noreply.github.com>
2019-12-26 16:42:50 +0800

    Update 接口.php

--- 接口.php
+++ 接口.php
@@ -1,9 +1,8 @@
 <?php
-/*    以下均未进行注入过滤，自行修改    */
-$option = $_GET['option']; //操作
-$key = $_GET['key']; //认证key
-if($key != '123')    die('Key认证失败'); //此处可以换成数据库检索或者数组检索
+$option = $_GET['option'];
+$key = $_GET['key'];
+if($key != '123')    die('Key认证失败'); 
 if($option == 'return'){
-    echo    json_encode(array('error'=>0,'message'=>'Ok')); //输出JSON
-} //继续其他操作
+    echo    json_encode(array('error'=>0,'message'=>'Ok')); 
+} 
 ?>
